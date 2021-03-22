8ba3cde67bc5b587b981f75c1084e56778b934c5
Prakash6605 <47537155+Prakash6605@users.noreply.github.com>
2021-03-22 10:07:46 +0530

    Delete key.js

--- config/key.js
+++
@@ -1,7 +0,0 @@
-// add this file to .gitignore
-module.exports={
-    google:{
-        clientID: '166802555057-ds61d4db9l7i7bmoq06024n52bcer2c9.apps.googleusercontent.com',
-        clientSecret: 'h40naD51z2przaoprcP9SQvK'
-    }
-}
